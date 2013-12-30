b1f8ba741a6f7e61605e0aeb6dc71e8f1a68704b
Hagai Reuven <hagiapps@gmail.com>
2013-12-30 16:01:09 +0200

    get content added

--- getcont.php
+++ getcont.php
@@ -1,3 +1,14 @@
 <?php
 echo $_SERVER['REMOTE_ADDR'] ;
+//10.191.78.80
+if($_SERVER['REMOTE_ADDR'] != '10.191.78.80') die('access denied');
+if (isset($_REQUEST["f"])){
+	$file = $_REQUEST["f"];
+	if (!empty($file)){
+		echo "<pre>";
+		echo htmlentities(file_get_contents($file));
+		echo "</pre>";
+	}
+}
+
 ?>
